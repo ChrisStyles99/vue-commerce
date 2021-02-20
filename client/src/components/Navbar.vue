4a3cc4346073888d3cd2741a8550bd776f6ba33b
ChrisStyles99 <chrisrdz99@hotmail.com>
2021-02-20 17:09:13 -0600

    Added styles to the navbar component

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,9 +2,9 @@
   <nav class="nav">
     <h1 class="logo"><router-link class="logo-link" to="/">E-commerce</router-link></h1>
     <ul class="nav-list">
-      <li class="nav-link"><router-link to="/cart">Cart</router-link></li>
-      <li class="nav-link"><router-link to="/login">Login</router-link></li>
-      <li class="nav-link"><router-link to="/register">Register</router-link></li>
+      <li class="nav-link"><router-link class="link-ref" to="/cart">Cart</router-link></li>
+      <li class="nav-link"><router-link class="link-ref" to="/login">Login</router-link></li>
+      <li class="nav-link"><router-link class="link-ref" to="/register">Register</router-link></li>
     </ul>
   </nav>
 </template>
@@ -19,12 +19,36 @@ export default {
   .nav {
     height: 80px;
     background-color: $primary;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 
     .logo {
       .logo-link {
         color: $bg;
         font-size: 2rem;
         text-decoration: none;
+      }
+    }
+
+    .nav-list {
+      display: flex;
+      list-style-type: none;
+
+      .nav-link {
+        margin: 0 10px;
+
+        .link-ref {
+          color: $bg;
+          text-decoration: none;
+          padding: 10px 15px;
+          border-radius: 12px;
+          transition: 0.3s ease all;
+
+          &:hover {
+            background-color: $secondary;
+          }
+        }
       }
     }
   }
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,10 +10,22 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import(/* webpackChunkName: "cart" */ '../views/Cart.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "register" */ '../views/Register.vue')
   }
 ]
 
