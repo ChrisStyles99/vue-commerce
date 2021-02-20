d214cc47e463a5d6a8b334ae6326af5edad80509
ChrisStyles99 <chrisrdz99@hotmail.com>
2021-02-20 16:49:35 -0600

    Added a Navbar component

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="nav">
+    <h1 class="logo"><router-link class="logo-link" to="/">E-commerce</router-link></h1>
+    <ul class="nav-list">
+      <li class="nav-link"><router-link to="/cart">Cart</router-link></li>
+      <li class="nav-link"><router-link to="/login">Login</router-link></li>
+      <li class="nav-link"><router-link to="/register">Register</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .nav {
+    height: 80px;
+    background-color: $primary;
+
+    .logo {
+      .logo-link {
+        color: $bg;
+        font-size: 2rem;
+        text-decoration: none;
+      }
+    }
+  }
+</style>
