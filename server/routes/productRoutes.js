8bd244ee0d184cc401177594bb469097efede74b
ChrisStyles99 <chrisrdz99@hotmail.com>
2021-02-21 11:30:56 -0600

    Added api route to get all distinct product categories

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const { getAll, getSingle } = require('../controllers/productController');
+const { getAll, getSingle, getCategories } = require('../controllers/productController');
 const router = Router();
 
 router.get('/all', getAll);
 router.get('/:id', getSingle);
+router.get('/categories', getCategories);
 router.post('/order');
 
 module.exports = router;
